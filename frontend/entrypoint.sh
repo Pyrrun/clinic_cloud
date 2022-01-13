057c092e0d510e8172edd021a99dc52deefdb6c2
test <test>
2022-01-13 19:07:31 +0100

    changes to auth

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -7,8 +7,9 @@ echo "Replacing env constants in JS"
 for file in $ROOT_DIR/js/app.*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
 do
   echo "Processing $file ...";
-  sed -i 's|VUE_APP_BASE_URL|'${VUE_APP_BASE_URL}'|g' $file
 
+  sed -i 's|VUE_APP_BASE_URL|'${VUE_APP_BASE_URL}'|g' $file
+  sed -i 's|VUE_APP_AUTH_URL|'${VUE_APP_AUTH_URL}'|g' $file
 done
 fi
 exec $@
